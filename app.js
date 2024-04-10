425f44216de4284126296fff15a7b204bddcde77
Jashan <141420286+MrMejor@users.noreply.github.com>
2024-04-10 11:19:30 +0000

    Added author names and email

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 /**
- * @author Sergio Fontan Llamas
- * @email sfontan@profesores.imf.com
+ * @author Naiara Zhiyao Lezameta Rodrigo
+ * @email nlezametar538@alumnos.imf.com
+ * @author Jashanjeet Singh Kuka
+ * @email kjashanjeets739@alumnos.imf.com
  */
