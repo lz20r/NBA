31423a9e4666b416165cb0d7982c183de7a61f88
lz20r <zhiyaonaiara@gmail.com>
2024-04-10 13:40:18 +0200

    Eliminar archivos innecesarios

---
+++ js/app.js
@@ -0,0 +1,12 @@
+/**
+ * @author Naiara Zhiyao Lezameta Rodrigo
+ * @email nlezametar538@alumnos.imf.com
+ * @author Jashanjeet Singh Kuka
+ * @email kjashanjeets739@alumnos.imf.com
+ */
+
+fetch('/json/franquicias.json')
+    .then(response => response.json())
+    .then(data => {   
+    })
+    .catch(error => console.error("Error ",error));
