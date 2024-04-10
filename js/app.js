b7e5334b6b355bd07e483c3021806c9ccc7fb9e1
MrMejor <jashanj2001@gmail.com>
2024-04-11 01:11:11 +0200

    Fetched json file but having problem with CreateElement

--- js/app.js
+++ js/app.js
@@ -6,8 +6,54 @@
  */
 
 fetch('/json/franquicias.json')
-    .then(response => response.json())
-    .then(data => {   
-        
-    })
-    .catch(error => console.error("Error ",error));
+    .then(data => data.json())
+    .then(data => {
+    pintarDatos(data);
+})
+    .catch(error => console.log("Error, no carga correctamente"));
+
+    function pintarDatos(data){
+        console.log(data["franquicias"]);
+    
+        let div_content = document.getElementById('content');
+    
+        for(let fran in data["franquicias"]){
+            // Por cada iteracion, creo la card y la coloco
+    
+            // 1. crear la card
+            let div = document.createElement('div');
+            div.classList.add('card');
+    
+            // 2. Crear etiqueta h1
+            let h1 = document.createElement('h1');
+            h1.textContent = "Nombre: " + data["franquicias"][fran]["nombre"];
+            div.appendChild(h1);
+    
+            // 3. Crear etiqueta h5 de ciudad.
+            let ciudad = document.createElement('h5');
+            h5.textContent = "Ciudad: " + data["franquicias"][fran]["ciudad"];
+            div.appendChild(ciudad);
+    
+            // 4. Crear etiqueta h5 de estado.
+            let estado = document.createElement('h5');
+            h5.textContent = "Estado: " + data["franquicias"][fran]["estado"];
+            div.appendChild(estado);
+    
+            // 5. Crear etiqueta h5 de conferencia.
+            let conferencia = document.createElement('h5');
+            h5.textContent = "Conferencia: " + data["franquicias"][fran]["conferencia"];
+            div.appendChild(conferencia);
+    
+            // 6. Crear etiqueta h5 de division.
+            let division = document.createElement('h5');
+            h5.textContent = "Division: " + data["franquicias"][fran]["division"];
+            div.appendChild(division);
+    
+            // 7. Crear etiqueta h5 de fundado.
+            let fundado = document.createElement('h5');
+            h5.textContent = "Fundado: " + data["franquicias"][fran]["fundado"];
+            div.appendChild(fundado);
+
+            div_content.appendChild(div);
+        }
+    }
